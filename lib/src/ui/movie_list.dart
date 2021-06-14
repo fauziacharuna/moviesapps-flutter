76c304d5da7151a3c75522c8e9644a9e46275373
fauziacharuna <fauziharuna@gmail.com>
2021-06-14 09:10:57 +0800

    Fixing bloc calling state

--- lib/src/ui/movie_list.dart
+++ lib/src/ui/movie_list.dart
@@ -2,11 +2,31 @@ import 'package:flutter/material.dart';
 import '../models/item_model.dart';
 import '../blocs/movies_bloc.dart';
 
-class MovieList extends StatelessWidget{
+class MovieList extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MovieListState();
+  }
+}
+
+class MovieListState extends State<MovieList> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    bloc.fetchAllMovies();
+  }
+
+  @override
+  void dispose() {
+    bloc.dispose();
+    // TODO: implement dispose
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    bloc.fetchAllMovies();
     return Scaffold(
       appBar: AppBar(
         title: Text('Popular Movies'),
@@ -27,13 +47,15 @@ class MovieList extends StatelessWidget{
 
   Widget buildList(AsyncSnapshot<ItemModel> snapshot) {
     return GridView.builder(
-        itemCount: snapshot.data!.results.length,
+        itemCount: snapshot.data?.results.length,
         gridDelegate:
             new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
         itemBuilder: (BuildContext context, int index) {
-          return Image.network(
-            'https://image.tmdb.org/t/p/w185${snapshot.data!.results[index].poster_path}',
-            fit: BoxFit.cover,
+          return GridTile(
+            child: Image.network(
+              'https://image.tmdb.org/t/p/w185${snapshot.data?.results[index].poster_path}',
+              fit: BoxFit.cover,
+            ),
           );
         });
   }
